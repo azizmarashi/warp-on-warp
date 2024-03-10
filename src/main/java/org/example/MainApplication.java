31078f2cb9aff029883c622f3a2a6c3df5559875
Aziz Hosseini <sam.hosseini256@gmail.com>
2024-03-10 05:52:58 +0330

    get best ip and result.csv

--- src/main/java/org/example/MainApplication.java
+++ src/main/java/org/example/MainApplication.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import lombok.SneakyThrows;
-
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -14,23 +13,21 @@ public class MainApplication {
         ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", "curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh | bash");
         Process process = processBuilder.start();
 
-
-        InputStream inputStream = process.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
-        }
-
         process.getOutputStream().write("1\n".getBytes());
         process.getOutputStream().flush();
 
-
-        InputStream inputStream2 = process.getInputStream();
+        InputStream inputStream2 = process.getErrorStream();
         BufferedReader reader2 = new BufferedReader(new InputStreamReader(inputStream2));
         String line2;
         while ((line2 = reader2.readLine()) != null) {
             System.out.println(line2);
+        }
+
+        InputStream inputStream3 = process.getInputStream();
+        BufferedReader reader3 = new BufferedReader(new InputStreamReader(inputStream3));
+        String line3;
+        while ((line3 = reader3.readLine()) != null) {
+            System.out.println(line3);
         }
 
     }
